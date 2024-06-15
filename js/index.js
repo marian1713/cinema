7ac7314afd75e4962df52b19a93c3bb0c1889325
marian1713 <navarromariana148@gmail.com>
2024-06-14 23:03:41 -0300

    rediseño de pagina

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// console.log(tempData);
+
+$(document).ready(function () {
+    const movieContainer = document.getElementById("containerMovies");
+
+    $.get("https://students-api.up.railway.app/movies", (data, error) => {
+        if (!data) {
+            alert("Ah ocurrido un error", error)
+        }
+
+        data.forEach((movie) => {
+            const moviesCard = document.createElement("div");
+            moviesCard.classList.add("moviesCard");
+            moviesCard.innerHTML = `
+            <h2>Director: ${movie.title}</h2>
+            <img src="${movie.poster}" alt="${movie.title}">
+            <p>Año: ${movie.year}</p>
+            <div class="movieDetails ">
+            <p>Director: ${movie.director}</p>
+            <p>Duración: ${movie.duration}</p>
+            <p>Género: ${movie.genre}</p>
+    <p>Puntuación: ${movie.rate}</p>
+    </div>
+    `;
+            movieContainer.appendChild(moviesCard);
+        });
+    })
+})
